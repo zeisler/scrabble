519f7e5381734dca5370fdfeac93f58a7f0e3cc9
Dustin Zeisler <dustin@zive.me>
2013-07-10 18:09:18 -0700

    change routes to have only two entry points into app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 ScrabbleRails::Application.routes.draw do
-  get "scrabble/index"
-  resources :dictionaries
   get 'word/:word' => 'dictionaries#show'
+  get '/' => 'scrabble#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
