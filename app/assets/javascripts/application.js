f64249f3c2ed4fc754eb2b4f85846acbb6fb78a0
Dustin Zeisler <dustin@zive.me>
2013-07-24 11:17:55 -0700

    add files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+ //= require jquery
+ //= require jquery.ui.all
+ //= require jquery_ujs
+ //= require turbolinks
 //= require bootstrap
-//= require angular
-//= require app/controllers/rack
+//= require_tree .
 
 
